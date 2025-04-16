f327e1ee478e64c6d24554125712e18023c66786
uzair-dev123 <uzairhassankhan@gmail.com>
2025-04-17 00:12:08 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-2xl text-blue-700">
+      InstantLetter.ai is working!
+    </div>
+  )
+}
+
+export default App
